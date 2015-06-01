537191375904195ff1ff106fc70530f84bba5ab0
Noah Keen <noahkeen@berkeley.edu>
2015-06-01 15:27:53 -0700

    Fixed stuff thanks to Raj

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -6,9 +6,22 @@ angular.module('myApp.view1', ['ngRoute'])
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
-  });
+});
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
+
+    $scope.print_text = function(){
+        if ($scope.text == 4 || $scope.text == 8 || $scope.text == 16) {
+            $scope.changeView = function(view){
+                $location.path(view);
+            }
+        } else {
+            alert("Please input 4, 8 or 16 into the search box.")
+        }
+    }
+
+    
+
 
 }]);
